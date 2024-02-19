d9185975452a3501b96636d77c5b246dbf5d55d3
root <root@ip-172-31-17-124.ap-southeast-1.compute.internal>
2024-02-19 08:16:40 +0000

    imran mavenfile

---
+++ src/main/java/test/app/im.java
@@ -0,0 +1,12 @@
+/**
+ * Hello world!
+ *
+ */
+public class im
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is first program using maven!" );
+    }
+}
+
